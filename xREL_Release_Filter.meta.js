0eabac6a7052280ca7cc658f80dbbcc006a05112
0657code <0657code@users.noreply.github.com>
2015-07-22 15:58:14 +0200

    Create xREL_Release_Filter.meta.js

---
+++ xREL_Release_Filter.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        xREL Release Filter
+// @namespace   tag:https://github.com/0657code/greasemonkey-xrel-filter,2015-07-16
+// @description Release filter for xREL. Also hides the "new comments" box upon clicking on its title. Credits to GarionCZ for his SteamGifts filter script.
+// @author      0657_Code
+// @include     http://www.xrel.to/movie/*/releases.html*
+// @include     http://www.xrel.to/tv/*/releases.html*
+// @downloadURL https://github.com/0657code/greasemonkey-xrel-filter/raw/master/xREL_Release_Filter.user.js
+// @updateURL   https://github.com/0657code/greasemonkey-xrel-filter/raw/master/xREL_Release_Filter.meta.js
+// @version     1.0
+// @grant       GM_getValue
+// @grant       GM_setValue
+// ==/UserScript==
